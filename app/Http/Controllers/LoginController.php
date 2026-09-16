bbd51b6bc6ee95464a7801bfe16b09b6d7149a64
AbdulBasitx19 <abdulbasitchaudhry106@gmail.com>
2026-09-16 22:11:19 +0500

    feat: create LoginController with showLoginForm function

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\User;
+
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+
+    // Show the login form (GET Request)
+    public function showLoginForm()
+    {
+        return view('auth.login');
+    }
+
+    
+
+}
