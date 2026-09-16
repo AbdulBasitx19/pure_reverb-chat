277fa6f7a472da5972da05ac8b75be169c5be85f
AbdulBasitx19 <abdulbasitchaudhry106@gmail.com>
2026-09-16 22:27:39 +0500

    feat: create Login function with session regenrate in controller

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth; // ✅ 1. Auth facade import kiya
 
 class LoginController extends Controller
 {
@@ -12,6 +13,28 @@ class LoginController extends Controller
     public function showLoginForm()
     {
         return view('auth.pages.login');
+    }
+
+    //handle login request (POST request)
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:6',
+        ]);
+
+        $credentials = $request->only('email', 'password'); // sirf email and password nikala
+
+        // Auth::attempt: Database mein check karo ke email/password match karte hain
+        if (Auth::attempt($credentials))
+        {
+            $request->sesssion()->regenerate(); // Session regenerate karo (security ke liye)
+            return redirect()->intended('/dashboard');
+        }
+
+        return back()->withErrors([
+            'email' => 'email or password is wrong.',
+        ])->onlyInput('email'); // Sirf email field ko wapis bheja (password nahi)
     }
 
 
